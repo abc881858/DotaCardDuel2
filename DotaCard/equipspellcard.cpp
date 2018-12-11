1baf2d852fe06dd624ac967168f9d7b6ae2539d2
吴冬亮 <635517016@qq.com>
2018-12-11 17:35:04 +0800

    Update equipspellcard.cpp

--- DotaCard/equipspellcard.cpp
+++ DotaCard/equipspellcard.cpp
@@ -1,4 +1,4 @@
-﻿#include "equipspellcard.h"
+#include "equipspellcard.h"
 #include "dota.h"
 #include <QMessageBox>
 #include <QDebug>
@@ -38,4 +38,24 @@ bool EquipSpellCard::testEffectFromFieldground()
 void EquipSpellCard::standby()
 {
     //准备阶段自动做的事, 每回合一次在这初始化
+}
+
+void EquipSpellCard::beforeEquip()
+{
+    qDota->currentActiveCard = this;
+    activeSpellCard();
+
+    CardMoveStruct move;
+    move.areaFrom = Card::Hand_Area;
+    move.areaTo = Card::Fieldground_Area;
+    move.indexFrom = qDota->getCardIndex(this);
+    move.indexTo = qDota->testPlace(Card::Fieldground_Area);
+    move.reason = CardMoveStruct::REASON_activeSpellCard;
+    qDota->moveCard(move);
+
+    qDota->whoIsDoing = false;
+
+    emit qDota->showChainAnimation(move.indexTo, Card::Fieldground_Area);
+//    qNet->sendChained(move.indexTo, Card::Fieldground_Area); // 考虑下提前发送Net连锁动画
+
 }
